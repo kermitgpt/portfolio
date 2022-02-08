1f1336ffa07adcfc07f6c7309882bc20ff10c50f
Matt Grogan <mattgrogan.dev@gmail.com>
2022-02-08 14:23:16 -0700

    worked on about, projects, and data.js files

---
+++ portfolio/src/data.js
@@ -0,0 +1,28 @@
+export const projects = [
+  {
+    title: "Venue Finder",
+    subtitle: "JavaScript | HTML | CSS | Bootstrap | MapBox API | SeatGeek API",
+    description:
+      "Venue Finder allows a user to have one website they can access in order to find music venues in whatever city they would like to search. This provides the user with a list of the 9 most popular venues in the city and gives links and addresses for easy access to find tickets/events for that venue.",
+    image: "./project1.jpg",
+    link: "https://bit.ly/venuefinder",
+  },
+  {
+    title: "",
+    subtitle:
+      "JavaScript | NodeJS | ExpressJS | Tailwind | Handlebars | Sequelize",
+    description:
+      "My Health Journal allows someone to create patient profiles for themselves as well as family members/loved ones. The user can then keep track of various health related items including temperature, symptoms, medication time and dosage, etc. This allows the user to have a single place to go to store important health related notes for each of the patients they have created.",
+    image: "./project2.jpg",
+    link: "https://bit.ly/myhealthjournal",
+  },
+  {
+    title: "Code Companion",
+    subtitle:
+      "React | Tailwind | Auth0 | MongoDB | Mongoose | Apollo | NodeJS | ExpressJS | GraphQL",
+    description:
+      "Code-Companion is a web app that allows users to team up with people around the world to work on coding projects and share their experience. This app allows you to post projects you are looking for partners on or join projects other users have posted. When a project is posted the creator enters a description, specifies which language/stack they are working with, lists any additional technologies being used on project, adds a GibHub repo link, and posts either a slack channel or some other way to contact them. This allows users to filter by programming language to find something that best suits their skillset.  User's can also build a custom profile which lets them list all of their relevant skills, accomplishments, and years/months of experience.",
+    image: "./project3.jpg",
+    link: "https://bit.ly/codecompanion",
+  },
+];
