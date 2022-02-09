0bc6f8123b7b1a4a8cefcf635584084e35d0f878
Matt Grogan <mattgrogan.dev@gmail.com>
2022-02-09 13:56:43 -0700

    fixing build issue on netlify

---
+++ src/components/Resume.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto"></div>
+    </section>
+  );
+}
