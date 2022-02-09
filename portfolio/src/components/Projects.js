c2740d55d4a9cdbc318eef9d7baa5a21e4a2086c
Matt Grogan <mattgrogan.dev@gmail.com>
2022-02-09 13:23:30 -0700

    built out rest of website, added gifs instead of jpg

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -12,9 +12,8 @@ export default function Projects() {
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
+            Below is a list of projects I have built. Hover over to get a
+            project description and click to see the deployed website!
           </p>
         </div>
         <div className="flex flex-wrap -m-4">
--- portfolio/src/data.js
+++ portfolio/src/data.js
@@ -3,8 +3,8 @@ export const projects = [
     title: "Venue Finder",
     subtitle: "JavaScript | HTML | CSS | Bootstrap | MapBox API | SeatGeek API",
     description:
-      "Venue Finder allows a user to have one website they can access in order to find music venues in whatever city they would like to search. This provides the user with a list of the 9 most popular venues in the city and gives links and addresses for easy access to find tickets/events for that venue.",
-    image: "./project1.jpg",
+      "Venue Finder allows a user to have one website they can access in order to find music venues in whatever city they would like to search. This provides the user with a list of the 9 most popular venues in the city and gives links and addresses for easy access to find tickets/events for that venue. The map to the right of the search bar, created with the MapBox API, plots out the coordinates for all 9 of the venues generated from the search. This map also lets you zoom in to see more precise locations.that venue.",
+    image: "./project1.gif",
     link: "https://bit.ly/venuefinder",
   },
   {
@@ -12,8 +12,8 @@ export const projects = [
     subtitle:
       "JavaScript | NodeJS | ExpressJS | Tailwind | Handlebars | Sequelize",
     description:
-      "My Health Journal allows someone to create patient profiles for themselves as well as family members/loved ones. The user can then keep track of various health related items including temperature, symptoms, medication time and dosage, etc. This allows the user to have a single place to go to store important health related notes for each of the patients they have created.",
-    image: "./project2.jpg",
+      "My Health Journal allows someone to create patient profiles for themselves as well as family members/loved ones. The user can then keep track of various health related items including temperature, symptoms, medication time and dosage, etc. This allows the user to have a single place to go to store important health related notes for each of the patients they have created. There is a drop-down menu on the journal section allowing you to specify which patient you’d like to record data for.",
+    image: "./project2.gif",
     link: "https://bit.ly/myhealthjournal",
   },
   {
@@ -22,7 +22,24 @@ export const projects = [
       "React | Tailwind | Auth0 | MongoDB | Mongoose | Apollo | NodeJS | ExpressJS | GraphQL",
     description:
       "Code-Companion is a web app that allows users to team up with people around the world to work on coding projects and share their experience. This app allows you to post projects you are looking for partners on or join projects other users have posted. When a project is posted the creator enters a description, specifies which language/stack they are working with, lists any additional technologies being used on project, adds a GibHub repo link, and posts either a slack channel or some other way to contact them. This allows users to filter by programming language to find something that best suits their skillset.  User's can also build a custom profile which lets them list all of their relevant skills, accomplishments, and years/months of experience.",
-    image: "./project3.jpg",
+    image: "./project3.gif",
     link: "https://bit.ly/codecompanion",
   },
+];
+
+export const skills = [
+  "ReactJS",
+  "NodeJS",
+  "ExpressJS",
+  "GraphQL",
+  "SQL",
+  "Sequelize",
+  "MongoDB",
+  "Mongoose",
+  "TailwindCSS",
+  "Handlebars",
+  "JavaScript",
+  "HTML",
+  "CSS",
+  "Bootstrap",
 ];
